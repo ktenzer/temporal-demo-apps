b8b1ebd8d367fa0da3514e390f65c2074dd8ba7b
ktenzer <keith.tenzer@gmail.com>
2023-02-16 16:00:01 -0800

    fixed issue in bootstram params

--- helloworld/starter/main.go
+++ helloworld/starter/main.go
@@ -6,38 +6,64 @@ import (
 	"os"
 
 	"crypto/tls"
+	"crypto/x509"
 
 	"github.com/ktenzer/temporal-demo-apps/helloworld"
 	"go.temporal.io/sdk/client"
 )
 
 func main() {
-	// The client is a heavyweight object that should be created once per process.
+	// The client and worker are heavyweight objects that should be created once per process.
 
-	var c client.Client
-	var err error
-	var cert tls.Certificate
-
-	if os.Getenv("MTLS") == "false" {
-		c, err = client.Dial(client.Options{
-			HostPort:  os.Getenv("TEMPORAL_HOST_URL"),
-			Namespace: os.Getenv("TEMPORAL_NAMESPACE"),
-		})
-	} else {
-		cert, err = tls.LoadX509KeyPair(os.Getenv("TEMPORAL_TLS_CERT"), os.Getenv("TEMPORAL_TLS_KEY"))
-		if err != nil {
-			log.Fatalln("Unable to load certs", err)
-		}
-
-		c, err = client.Dial(client.Options{
-			HostPort:  os.Getenv("TEMPORAL_HOST_URL"),
-			Namespace: os.Getenv("TEMPORAL_NAMESPACE"),
-			ConnectionOptions: client.ConnectionOptions{
-				TLS: &tls.Config{Certificates: []tls.Certificate{cert}},
-			},
-		})
+	clientOptions := client.Options{
+		HostPort:  os.Getenv("TEMPORAL_HOST_URL"),
+		Namespace: os.Getenv("TEMPORAL_NAMESPACE"),
 	}
 
+	if os.Getenv("TEMPORAL_MTLS_TLS_CERT") != "" && os.Getenv("TEMPORAL_MTLS_TLS_KEY") != "" {
+		if os.Getenv("TEMPORAL_MTLS_TLS_CA") != "" {
+			caCert, err := os.ReadFile(os.Getenv("TEMPORAL_MTLS_TLS_CA"))
+			if err != nil {
+				log.Fatalln("failed reading server CA's certificate", err)
+			}
+
+			certPool := x509.NewCertPool()
+			if !certPool.AppendCertsFromPEM(caCert) {
+				log.Fatalln("failed to add server CA's certificate", err)
+			}
+
+			cert, err := tls.LoadX509KeyPair(os.Getenv("TEMPORAL_MTLS_TLS_CERT"), os.Getenv("TEMPORAL_MTLS_TLS_KEY"))
+			if err != nil {
+				log.Fatalln("Unable to load certs", err)
+			}
+
+			var serverName string
+			if os.Getenv("TEMPORAL_MTLS_TLS_ENABLE_HOST_VERIFICATION") == "true" {
+				serverName = os.Getenv("TEMPORAL_MTLS_TLS_SERVER_NAME")
+			}
+
+			clientOptions.ConnectionOptions = client.ConnectionOptions{
+				TLS: &tls.Config{
+					RootCAs:      certPool,
+					Certificates: []tls.Certificate{cert},
+					ServerName:   serverName,
+				},
+			}
+		} else {
+			cert, err := tls.LoadX509KeyPair(os.Getenv("TEMPORAL_MTLS_TLS_CERT"), os.Getenv("TEMPORAL_MTLS_TLS_KEY"))
+			if err != nil {
+				log.Fatalln("Unable to load certs", err)
+			}
+
+			clientOptions.ConnectionOptions = client.ConnectionOptions{
+				TLS: &tls.Config{
+					Certificates: []tls.Certificate{cert},
+				},
+			}
+		}
+	}
+
+	c, err := client.Dial(clientOptions)
 	if err != nil {
 		log.Fatalln("Unable to create client", err)
 	}
--- helloworld/worker/main.go
+++ helloworld/worker/main.go
@@ -22,32 +22,45 @@ func main() {
 	}
 
 	if os.Getenv("TEMPORAL_MTLS_TLS_CERT") != "" && os.Getenv("TEMPORAL_MTLS_TLS_KEY") != "" {
-		caCert, err := os.ReadFile(os.Getenv("TEMPORAL_MTLS_TLS_CA"))
-		if err != nil {
-			log.Fatalln("failed reading server CA's certificate", err)
-		}
+		if os.Getenv("TEMPORAL_MTLS_TLS_CA") != "" {
+			caCert, err := os.ReadFile(os.Getenv("TEMPORAL_MTLS_TLS_CA"))
+			if err != nil {
+				log.Fatalln("failed reading server CA's certificate", err)
+			}
 
-		certPool := x509.NewCertPool()
-		if !certPool.AppendCertsFromPEM(caCert) {
-			log.Fatalln("failed to add server CA's certificate", err)
-		}
+			certPool := x509.NewCertPool()
+			if !certPool.AppendCertsFromPEM(caCert) {
+				log.Fatalln("failed to add server CA's certificate", err)
+			}
 
-		cert, err := tls.LoadX509KeyPair(os.Getenv("TEMPORAL_MTLS_TLS_CERT"), os.Getenv("TEMPORAL_MTLS_TLS_KEY"))
-		if err != nil {
-			log.Fatalln("Unable to load certs", err)
-		}
+			cert, err := tls.LoadX509KeyPair(os.Getenv("TEMPORAL_MTLS_TLS_CERT"), os.Getenv("TEMPORAL_MTLS_TLS_KEY"))
+			if err != nil {
+				log.Fatalln("Unable to load certs", err)
+			}
 
-		var serverName string
-		if os.Getenv("TEMPORAL_MTLS_TLS_ENABLE_HOST_VERIFICATION") == "true" {
-			serverName = os.Getenv("TEMPORAL_MTLS_TLS_SERVER_NAME")
-		}
+			var serverName string
+			if os.Getenv("TEMPORAL_MTLS_TLS_ENABLE_HOST_VERIFICATION") == "true" {
+				serverName = os.Getenv("TEMPORAL_MTLS_TLS_SERVER_NAME")
+			}
 
-		clientOptions.ConnectionOptions = client.ConnectionOptions{
-			TLS: &tls.Config{
-				RootCAs:      certPool,
-				Certificates: []tls.Certificate{cert},
-				ServerName:   serverName,
-			},
+			clientOptions.ConnectionOptions = client.ConnectionOptions{
+				TLS: &tls.Config{
+					RootCAs:      certPool,
+					Certificates: []tls.Certificate{cert},
+					ServerName:   serverName,
+				},
+			}
+		} else {
+			cert, err := tls.LoadX509KeyPair(os.Getenv("TEMPORAL_MTLS_TLS_CERT"), os.Getenv("TEMPORAL_MTLS_TLS_KEY"))
+			if err != nil {
+				log.Fatalln("Unable to load certs", err)
+			}
+
+			clientOptions.ConnectionOptions = client.ConnectionOptions{
+				TLS: &tls.Config{
+					Certificates: []tls.Certificate{cert},
+				},
+			}
 		}
 	}
 
